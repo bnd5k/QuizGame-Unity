b05f95f776e4b947bba606db9dc5aa4942f9308c
Ben Downey <Ben.Downey@MutuallyHuman.com>
2017-03-01 07:03:11 -0800

    Adds GameEditor class. Pt 2, vid 5.

---
+++ Assets/Scripts/Editor/GameDataEditor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class GameDataEditor : EditorWindow {
+
+	public GameData gameData;
+
+	private string gameDataProjectFilePath = "/StreamingAssets/data.json";
+
+	private void LoadGameData() {
+		string filePath = Application.dataPath + gameDataProjectFilePath;
+
+		if (File.Exists (filePath)) {
+			string dataAsJson = File.ReadAllText (filePath);
+			gameData = JsonUtility.FromJson<GameData> (dataAsJson);
+
+		} else {
+			gameData = new GameData ();
+		}
+	}
+
+	private void SaveGameData() {
+		string dataAsJson = JsonUtility.ToJson (gameData);
+		string filePath = Application.dataPath + gameDataProjectFilePath;
+		File.WriteAllText (filePath, dataAsJson);
+	}
+}
